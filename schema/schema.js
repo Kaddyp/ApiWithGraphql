1602c5eb046ab2faed1b585b551466e805e36815
Kalyani Pawar <kalyani.pawar@tslproducts.com>
2022-08-18 13:59:40 +0100

    Initial Commit

---
+++ schema/schema.js
@@ -0,0 +1,40 @@
+const graphql = require('graphql');
+const _ = require('lodash');
+
+const {GraphQLObjectType,
+       GraphQLString,
+       GraphQLInt,
+       GraphQLSchema} = graphql
+
+const users = [
+    {"id": "23", "firstName":"Bill", "age":20},
+    {"id": "47", "firstName":"John", "age":22},
+    {"id": "13", "firstName":"Andy", "age":11},
+    {"id": "76", "firstName":"Kerio", "age":33}
+]
+
+const UserType = new GraphQLObjectType({
+    name: 'User',
+    fields:{
+        id:{type: GraphQLString},
+        firstName:{type: GraphQLString},
+        age:{type: GraphQLInt}
+    }
+})
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields:{
+        user:{
+            type: UserType,
+            args:{id:{type:GraphQLString}},
+            resolve(parentValue, args){
+                return _.find(users,{id:args.id})
+            }
+        }
+    }
+})
+
+module.exports = new GraphQLSchema({
+    query:RootQuery
+})
